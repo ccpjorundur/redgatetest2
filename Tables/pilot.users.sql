3f0d15fa63a64ab6fa8490468e7c72d9ce2902f5
ccpjorundur <jorundur@ccpgames.com>
2014-09-05 12:40:28 +0000

    Adding column and changeing static data

--- Tables/pilot.users.sql
+++ Tables/pilot.users.sql
@@ -5,7 +5,8 @@ CREATE TABLE [pilot].[users]
 [createDate] [datetime2] (0) NOT NULL CONSTRAINT [DF__users__createDat__412EB0B6] DEFAULT (getutcdate()),
 [homePhone] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [workPhone] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[gmsPhone] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[gmsPhone] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[address] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [pilot].[users] ADD CONSTRAINT [users_PK] PRIMARY KEY CLUSTERED  ([userID]) ON [PRIMARY]
