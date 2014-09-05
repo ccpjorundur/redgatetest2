7c5956a7eb5a38fab103a44f27905bc23b7461fa
ccpjorundur <jorundur@ccpgames.com>
2014-09-05 12:22:41 +0000

    Adding pilot.users.*Phone columns

--- Tables/pilot.users.sql
+++ Tables/pilot.users.sql
@@ -2,7 +2,10 @@ CREATE TABLE [pilot].[users]
 (
 [userID] [int] NOT NULL IDENTITY(1, 1),
 [userName] [nvarchar] (200) COLLATE Latin1_General_CI_AI NOT NULL,
-[createDate] [datetime2] (0) NOT NULL CONSTRAINT [DF__users__createDat__412EB0B6] DEFAULT (getutcdate())
+[createDate] [datetime2] (0) NOT NULL CONSTRAINT [DF__users__createDat__412EB0B6] DEFAULT (getutcdate()),
+[homePhone] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[workPhone] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[gmsPhone] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [pilot].[users] ADD CONSTRAINT [users_PK] PRIMARY KEY CLUSTERED  ([userID]) ON [PRIMARY]
